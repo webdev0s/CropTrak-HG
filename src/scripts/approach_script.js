746b9e153e2789a948e13ab42ed3967ac9c62110
Gustavo Scarcelli Lucianelli <104444836+gustavoscarl@users.noreply.github.com>
2023-09-01 01:02:55 -0300

    approach page changes

---
+++ src/scripts/approach_script.js
@@ -0,0 +1,8 @@
+// menu dropdown
+
+const dropdown = document.getElementById('dropdown');
+const menu = document.getElementById('menu');
+
+dropdown.addEventListener('click', function() {
+  menu.classList.toggle('hidden')
+});
